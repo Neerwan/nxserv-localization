fdb5644ce8861f230b471513233aef7ce10630af
nx <noiwex1911@gmail.com>
2019-04-02 09:51:15 +0300

    ldscr and reports

--- all/ldscr/locale.js
+++ all/ldscr/locale.js
@@ -12,14 +12,10 @@ var langs = {
 		bsp: "Полоса загрузки может стоять на месте или возвращаться назад - это нормально",
 		resources: "Подгружаем ресурсы",
 		hints: [
-			"Если проверка Workshop аддонов идёт необычно очень долго, то закройте GMod и перезагрузите Steam.",
-			"Если произошел креш, не волнуйтесь: сервер быстро перезагрузится, а вещи, профессия и местоположение будут вам возвращены автоматически.",
-			"Обязательно ознакомьтесь с правилами, а также читайте новости. В них нередко содержится важная информация.",
-			"Игра на серверах NxServ абсолютно бесплатна и требует лишь лицензионную копию Garry's Mod, однако вы можете порадовать себя специальными предметами в нашем магазине. Для некоторых режимов вроде NxRP (бывш. DarkRP) возможна покупка Премиум-подписки на месяц. Премиум сделает вашу игру приятной и удобной.",
-			'На всех серверах NxServ доступны 100% наши, с любовью сделанные аксессуары для вашего игрового персонажа. Ознакомиться с ассортиментом магазина можно в меню F4 (для DarkRP) или "Магазин" в ESC-меню.',
-			"При постройке старайтесь использовать наименьшее количество объектов: вместо нескольких маленьких пропов возьмите один большой.",
-			"Если вашей постройке из дупликатора не нужно двигаться (дом, магазин, забор и т.д.), загружайте ее с отмеченной галкой \"Paste w/o constraints\". Это уберет соединения и заморозит постройку, благодаря чему на сервер не будет лишней нагрузки от того, что все равно не должно двигаться.",
-		]
+
+		],
+		ac_warning: "Использование читов строго запрещено и карается неснимаемым баном",
+		ac_frags: "Поймано нарушителей: ",
 	},
 	en: {
 		init: "Loading",
@@ -34,8 +30,10 @@ var langs = {
 		bsp: "Loading bar can stand still or reset - don't worry, it is fine",
 		resources: "Loading resources",
 		hints: [
-			"Our American server is pretty new. Please invite your friends and have fun!"
-		]
+
+		],
+		ac_warning: "Use of cheats is strictly prohibited and is punished with unremovable ban",
+		ac_frags: "Caught violators: ",
 	},
 	fr: {
 		init: "Chargement en cours...",
@@ -50,15 +48,9 @@ var langs = {
 		bsp: "Si la barre de chargement apparaît parfois immobile ou bloquée, ne vous inquiétez pas, tout va bien",
 		resources: "Chargement des ressources",
 		hints: [
-			"Si la vérification des addons du Workshop est trop longue, fermez Garry's Mod et redémarrez Steam.",
-			"Vous n'êtes pas limités aux métiers de la liste du menu F4, vous pouvez très bien être propriétaire de cinéma ou revendeur de biens.",
-			"En cas de panne de serveur, ne vous inquiétez pas - le serveur se redémarrera en peu de temps et vous aurez tous vos objets, votre emploi et position restaurés automatiquement.",
-			"Assurez-vous d'avoir bien lu les règles. Vous pouvez également lire nos nouveautés - il y a toujours quelque chose d'intéressant au sujet de prochaines mises à jour et plus.",
-			"Jouer sur NxServ est totalement gratuit, la seule chose dont vous avez besoin est une copie légale de Garry's Mod. Toutefois vous pouvez toujours vous faire plaisir en achetant les objets spéciaux de notre boutique. Pour le mode de jeu NxRP il est possible d'acheter un abonnement Premium d'un mois. Le Premium rend votre expérience plaisante, facile et confortable.",
-			"Nous avons créé d'adorables accessoires pour votre personnage en jeu, que vous pouvez porter sur chacun de nos serveurs. Pour voir la liste complète d'objets, utilisez le menu F4 (sur DarkRP) ou \"Boutique\" dans le menu Échap.",
-			"Lorsque vous construisez, essayez d'utiliser le moins de props possible : au lieu d'utiliser plein de petits props, utilisez-en un gros !",
-			"S'il arrivait que vous soyez bloqués dans un prop ou dans un mur, écrivez !stuck dans le chat.",
-			// "Parlez vous anglais ? Aidez-nous à traduire notre serveur en français. Visitez https://nxserv.eu/fr",
-		]
+
+		],
+		ac_warning: "Use of cheats is strictly prohibited and is punished with unremovable ban",
+		ac_frags: "Caught violators: ",
 	}
 }
---
+++ all/nxlocale/report.lua
@@ -0,0 +1,25 @@
+return {
+	en = {
+		title = "Report a problem",
+		entry_label = "Specify your problem:",
+		send_button = "Send",
+		thanks = "Thank you!",
+		too_short = "Message is too short",
+	},
+
+	ru = {
+		title = "Сообщить о проблеме",
+		entry_label = "Опишите свою проблему:",
+		send_button = "Отправить",
+		thanks = "Спасибо!",
+		too_short = "Сообщение слишком короткое",
+	},
+
+	fr = {
+		title = "Report a problem",
+		entry_label = "Specify your problem:",
+		send_button = "Send",
+		thanks = "Thank you!",
+		too_short = "Message is too short",
+	},
+}
